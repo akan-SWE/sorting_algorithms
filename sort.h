86d76520786b9ee39a1f83878c0d08c8745c6f5c
akanSWE <a.akanSWE@gamil.com>
2024-02-20 12:37:10 +0100

    add: header file

---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void print_array(int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void swap(int *array, size_t index);
+#endif /* SORT_H */

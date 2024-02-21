d83eef45e990229d22c9205513f0f77c6e5a1d87
akanSWE <a.akanSWE@gamil.com>
2024-02-21 03:45:54 +0100

    fix: handle printing of array properly

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,22 +11,29 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, least;
+	bool found_min_num = false;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; array && i < size; i++)
+	for (i = 0; array && i < size - 1; i++)
 	{
 		int temp = array[i];
 
 		for (j = i, least = i; j < size - 1; j++)
 		{
 			if (array[j + 1] < array[least])
+			{
 				least = j + 1; /* current smallest element */
+				found_min_num = true;
+			}
+
 		}
 		array[i] = array[least];
 		array[least] = temp;
 
-		print_array(array, size);
+		if (found_min_num)
+			print_array(array, size);
+		found_min_num = false;
 	}
 }
